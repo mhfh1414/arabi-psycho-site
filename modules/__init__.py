b4b9dfb71bb58be66b37616ecae6c620f89128fb
mhfh1414 <mhfh26@gmail.com>
2025-09-01 11:45:20 +0300

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,5 @@
+# تهيئة حزمة modules
+
 from .tests_psych import PSYCH_TESTS, score_test as score_psych
 from .tests_personality import PERS_TESTS, score_personality
 from .recommend import recommend_tests_from_case
