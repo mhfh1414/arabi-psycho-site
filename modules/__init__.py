9f91a6ba6b672af30672f34db617c7bcf3323947
mhfh1414 <mhfh26@gmail.com>
2025-09-01 11:25:31 +0300

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,13 +1,9 @@
-# يجعل المجلد modules حزمة بايثون
-
 from .tests_psych import PSYCH_TESTS, score_test as score_psych
 from .tests_personality import PERS_TESTS, score_personality
 from .recommend import recommend_tests_from_case
 
 __all__ = [
-    "PSYCH_TESTS",
-    "score_psych",
-    "PERS_TESTS",
-    "score_personality",
+    "PSYCH_TESTS", "score_psych",
+    "PERS_TESTS", "score_personality",
     "recommend_tests_from_case",
 ]
