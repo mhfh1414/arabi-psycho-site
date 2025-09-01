5d67edcee718526f8054b9dcb509962e119655dc
mhfh1414 <mhfh26@gmail.com>
2025-09-01 09:34:59 +0300

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,9 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Package initializer for modules
-هذا الملف يجعل مجلد modules يُعامل كحزمة بايثون.
-"""
-
+# يجعل المجلد باكيج ويسهّل الاستيراد
 from .tests_psych import PSYCH_TESTS, score_test as score_psych
 from .tests_personality import PERS_TESTS, score_personality
 from .recommend import recommend_tests_from_case
+
+__all__ = [
+    "PSYCH_TESTS",
+    "score_psych",
+    "PERS_TESTS",
+    "score_personality",
+    "recommend_tests_from_case",
+]
