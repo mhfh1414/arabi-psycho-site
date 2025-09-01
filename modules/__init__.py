9d71d177c3d287f9a271abd959091b8311ff3266
mhfh1414 <mhfh26@gmail.com>
2025-09-01 09:18:05 +0300

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,9 @@
-# يجعل مجلد modules حزمة بايثون
+# -*- coding: utf-8 -*-
+"""
+Package initializer for modules
+هذا الملف يجعل مجلد modules يُعامل كحزمة بايثون.
+"""
+
+from .tests_psych import PSYCH_TESTS, score_test as score_psych
+from .tests_personality import PERS_TESTS, score_personality
+from .recommend import recommend_tests_from_case
