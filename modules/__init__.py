69ac65684d48d7ed2e79978d79252dbcde8c97ab
mhfh1414 <mhfh26@gmail.com>
2025-09-03 08:48:02 +0300

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,12 +1,4 @@
-# نجعل الاستيراد نسبياً من داخل نفس المجلد
 from .tests_psych import PSYCH_TESTS, score_test as score_psych
 from .tests_personality import PERS_TESTS, score_personality
 from .recommend import recommend_tests_from_case
-
-__all__ = [
-    "PSYCH_TESTS",
-    "score_psych",
-    "PERS_TESTS",
-    "score_personality",
-    "recommend_tests_from_case",
-]
+__all__ = ["PSYCH_TESTS", "score_psych", "PERS_TESTS", "score_personality", "recommend_tests_from_case"]
