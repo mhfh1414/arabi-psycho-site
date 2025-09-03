cea56b6805fe7990321bcaf88c7fd68fc1dff882
mhfh1414 <mhfh26@gmail.com>
2025-09-03 23:03:26 +0300

    Update __init__.py

--- services/tests/__init__.py
+++ services/tests/__init__.py
@@ -1,5 +1,7 @@
-
-# يجعل services/tests حزمة قابلة للاستيراد
-# ويصدّر دوال أساسية للاستخدام السريع
 from .tests_psych import score_test, psych_info
-from .tests_personality import score_big5
+from .tests_personality import personality_test, personality_info
+
+__all__ = [
+    "score_test", "psych_info",
+    "personality_test", "personality_info",
+]
