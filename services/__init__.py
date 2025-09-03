0d0d3714972b950cb5c356399842884ba34788d1
mhfh1414 <mhfh26@gmail.com>
2025-09-03 17:53:49 +0300

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,4 +1,4 @@
-from .tests_psych import PSYCH_TESTS, score_test as score_psych
-from .tests_personality import PERS_TESTS, score_personality
-from .recommend import recommend_tests_from_case
-__all__ = ["PSYCH_TESTS", "score_psych", "PERS_TESTS", "score_personality", "recommend_tests_from_case"]
+# يجعل services مكتبة بايثون
+# ويجهز التصدير المباشر
+from . import tests
+from . import recommend
