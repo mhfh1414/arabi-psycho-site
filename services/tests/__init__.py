8bd93a348cd8316be94e5627ac1de8eda34c455e
mhfh1414 <mhfh26@gmail.com>
2025-09-04 08:25:19 +0300

    Delete services/tests/__init__.py

--- services/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-from .tests_psych import score_test, psych_info
-from .tests_personality import personality_test, personality_info
-
-__all__ = [
-    "score_test", "psych_info",
-    "personality_test", "personality_info",
-]
